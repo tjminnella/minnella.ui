08f20b32ecbd6ffaed34417956a6c2343684ee47
tjminnella <tjminnella@gmail.com>
2026-03-22 23:28:50 -0500

    Basic Template v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 
-
 import './styles/App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
@@ -7,8 +6,6 @@ import { Footer } from './components/Footer'
 import { Router } from './router'
 
 function App() {
-  
-
   return (
     <BrowserRouter>
       <Navbar />
